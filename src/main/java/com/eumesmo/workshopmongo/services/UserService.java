13315695cb6eb169a740397ba8c2021a3e7f13eb
Pedro Henrique <pedro.henrique59@outlook.com>
2020-08-28 17:32:58 -0300

    Obtendo um usuário por id

--- src/main/java/com/eumesmo/workshopmongo/services/UserService.java
+++ src/main/java/com/eumesmo/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.eumesmo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eumesmo.workshopmongo.domain.User;
 import com.eumesmo.workshopmongo.repository.UserRepository;
+import com.eumesmo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		if(user.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user.get();
 	}
 }
