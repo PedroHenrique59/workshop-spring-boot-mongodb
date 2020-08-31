6065a141e4879a81e50159fc69a76af6a40de101
Pedro Henrique <pedro.henrique59@outlook.com>
2020-08-31 16:29:02 -0300

    Inserindo usuário com post

--- src/main/java/com/eumesmo/workshopmongo/services/UserService.java
+++ src/main/java/com/eumesmo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eumesmo.workshopmongo.domain.User;
+import com.eumesmo.workshopmongo.dto.UserDTO;
 import com.eumesmo.workshopmongo.repository.UserRepository;
 import com.eumesmo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user.get();
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
