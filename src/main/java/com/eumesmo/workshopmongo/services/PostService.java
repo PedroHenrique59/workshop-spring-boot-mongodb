6dcea5e4e2a3de3bc769156b69a43002c58ea5d9
Pedro Henrique <pedro.henrique59@outlook.com>
2020-09-01 17:19:43 -0300

    Consulta simples com quey methods

--- src/main/java/com/eumesmo/workshopmongo/services/PostService.java
+++ src/main/java/com/eumesmo/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.eumesmo.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,9 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return post.get();
+	}
+
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
