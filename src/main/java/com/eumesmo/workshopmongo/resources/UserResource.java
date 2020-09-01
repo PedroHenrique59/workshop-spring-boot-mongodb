4e620f2d4f371c0aae58c2aa8837dee33e7c032a
Pedro Henrique <pedro.henrique59@outlook.com>
2020-09-01 16:38:32 -0300

    Obtendo Post por id

--- src/main/java/com/eumesmo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eumesmo/workshopmongo/resources/UserResource.java
@@ -65,4 +65,7 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj.getPosts());
 	}
+
+
+
 }
---
+++ src/main/java/com/eumesmo/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.eumesmo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eumesmo.workshopmongo.domain.Post;
+import com.eumesmo.workshopmongo.repository.PostRepository;
+import com.eumesmo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+		
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		if(post.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return post.get();
+	}
+}
